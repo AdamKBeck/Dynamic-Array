22b2ba652163c3a03bee6fe9e781f7b74803a3bb
Adam Beck <akb93@case.edu>
2016-06-25 21:04:50 -0400

    Defined init() function

--- DynamicArray.c
+++ DynamicArray.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-
 /* A Vector is a dynamically sized array */
-int main(){
 
-	return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include "DynamicArray.h"
+
+/* initialize size and capacity */
+void init(Vector *v){
+	v->size = 0;
+	v->capacity = INITIAL_CAPACITY;
+	v->data = malloc(sizeof(int) * INITIAL_CAPACITY);
 }
--- DynamicArray.h
+++ DynamicArray.h
@@ -1,12 +1,24 @@
 #ifndef DYNAMICARRAY_H_
 #define DYNAMICARRAY_H_
 
+#define INITIAL_CAPACITY 100 // initial capacity of vector size
+
 typedef struct{
 	int size; 		// elements used so far
 	int capacity; 	// total available elements
 	int *data;		// dynamic array data stored here
 } Vector;
 
+void init(Vector *v);
 
+void add(Vector *v, int value);
+
+int get(Vector *v, int index);
+
+void replace(Vector *v, int index, int value);
+
+void resize(Vector *vector);
+
+void free(Vector *vector);
 
 #endif /* DYNAMICARRAY_H_ */
